f96ba29c20756ed98c4704b86636fed0790ced56
Krom <krommail@gmail.com>
2018-11-24 19:01:01 +0300

    Bootstrap application

---
+++ src/main/kotlin/ru/grushetsky/fizzbuzzenterprise/FizzbuzzenterpriseApplication.kt
@@ -0,0 +1,11 @@
+package ru.grushetsky.fizzbuzzenterprise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FizzbuzzenterpriseApplication
+
+fun main(args: Array<String>) {
+    runApplication<FizzbuzzenterpriseApplication>(*args)
+}
---
+++ src/test/kotlin/ru/grushetsky/fizzbuzzenterprise/FizzbuzzenterpriseApplicationTests.kt
@@ -0,0 +1,16 @@
+package ru.grushetsky.fizzbuzzenterprise
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FizzbuzzenterpriseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
